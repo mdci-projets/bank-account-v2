8dfdc9c82964a37a9a81949c730ba738f0f6b119
youmassa <y.massaoudi@yahoo.fr>
2025-04-02 13:03:25 +0200

    style: Externalise les messages de validation dans un fichier de ressources

--- src/main/java/com/mdci/bankaccount/application/dto/BankOperationRequestDTO.java
+++ src/main/java/com/mdci/bankaccount/application/dto/BankOperationRequestDTO.java
@@ -5,8 +5,8 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 public record BankOperationRequestDTO(
-        @NotNull(message = "Le montant est obligatoire.")
-        @Positive(message = "Le montant doit être strictement positif.")
+        @NotNull(message = "{bank.operation.amount.notnull}")
+        @Positive(message = "{bank.operation.amount.positive}")
         BigDecimal amount
 ) {
 }
